7d0648bb7516ea65e2dbcf5abfc6369d7c146c1d
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-08-13 18:31:27 -0700

    delete appendLi function and hardcode Odin Project li

--- script.js
+++ script.js
@@ -80,14 +80,3 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.reload();
     }
 });
-
-
-function appendLi(textOfLi) {
-    let newLi = document.createElement("li");
-    newLi.textContent = textOfLi;
-
-    let existingListParentNode = document.querySelector('ul');
-    existingListParentNode.appendChild(newLi);
-} 
-
-appendLi("The Odin Project's \"setup git\" tutorial is very informative!");
